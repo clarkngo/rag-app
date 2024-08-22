81879ce8c4766079d7df3be7b018dbd588a41492
Clark Ngo <30854108+clarkngo@users.noreply.github.com>
2024-08-22 22:11:19 +0000

    update rag

--- query_data.py
+++ query_data.py
@@ -40,7 +40,7 @@ def query_rag(query_text: str):
     prompt = prompt_template.format(context=context_text, question=query_text)
     # print(prompt)
 
-    model = Ollama(model="mistral")
+    model = Ollama(model="gemma2:2b")
     response_text = model.invoke(prompt)
 
     sources = [doc.metadata.get("id", None) for doc, _score in results]
